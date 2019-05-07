b64756ff1d33df8b4def8be2eb606a8a81bfa9c0
gauthier <gauthierbouyjou@aol.com>
2019-05-07 18:30:32 +0200

    update

---
+++ displayAll.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    $dir = "motion/" . (string)$_GET['dir'];
+
+    $files = scandir("$dir");
+
+    foreach ($files as $file) {
+        if (fnmatch("*.jpg", $file)) {
+            echo "
+	    <a href=\"$dir/$file\" >
+		<img src=\"$dir/$file\" width=320 height=240 />
+	    </a>
+	    ";
+        }
+    }
+
+    ?>
+
+</body>
+
+</html>

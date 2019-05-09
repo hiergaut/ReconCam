3391fed8ac4171d81075f85b2fdfededf667d448
gauthier <gauthierbouyjou@aol.com>
2019-05-09 18:44:28 +0200

    update

--- displayAll.php
+++ displayAll.php
@@ -12,14 +12,16 @@
         if (fnmatch("*.jpg", $file)) {
             echo "
                 <a href=\"$dir/$file\" >
-                <img src=\"$dir/$file\" width=320 height=240 />
+                    <img src=\"$dir/$file\" width=320 height=240 />
                 </a>
                 ";
         }
 
         if (fnmatch("*.ogv", $file)) {
             echo "
+                <video controls>
                     <source src=\"$dir/$file\" type=video/ogg>
+                </video>
                 ";
         }
     }

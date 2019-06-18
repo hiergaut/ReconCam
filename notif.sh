7927cfa77b8915eca1f4db168bb66bf9bd561c3a
gauthier <gauthierbouyjou@aol.com>
2019-06-18 10:49:58 +0200

    update

--- notif.sh
+++ notif.sh
@@ -1,15 +1,36 @@
 #! /bin/bash -e
-
-
-if [ -z $1 ]; then
-    echo "empty parameter"
-    exit 1
-fi
-
-mkdir -p alert
-touch alert/$1
-sleep 18
-rm alert/$1
-
-find alert/ -empty -exec rm alert.jpg \;
-
+#
+#
+# if [ -z $1 ]; then
+#     echo "empty parameter"
+#     exit 1
+# fi
+#
+# mkdir -p alert
+# touch alert/$1
+# sleep 18
+# rm alert/$1
+#
+# find alert/ -empty -exec rm alert.jpg \;
+#
+if [ $# -ne 1 ]; then                                                                                                  
+        echo "usage : $0 event";                                                                                       
+        exit 1                                                                                                         
+fi                                                                                                                     
+                                                                                                                       
+rep="/var/www/html/ReconCam/"                                                                                          
+cd $rep                                                                                                                
+                                                                                                                       
+mkdir -p alert                                                                                                         
+touch alert/$1                                                                                                         
+                                                                                                                       
+sleep 18                                                                                                               
+                                                                                                                       
+rm alert/$1                                                                                                            
+                                                                                                                       
+                                                                                                                       
+# if [ -z "$(ls alert)" ]; then                                                                                        
+    # rm alert.jpg                                                                                                     
+# fi                                                                                                                   
+find alert/ -maxdepth 0 -empty -exec rm alert.jpg \;                                                                   
+                                                                                                                       
--- remote.sh
+++ remote.sh
@@ -1,5 +1,10 @@
 #! /bin/bash -e
 
+if [ $# -ne 1 ]; then                                                                                                  
+        echo "usage : $0 event";                                                                                       
+        exit 1                                                                                                         
+fi                                                                                                                     
+
 rep="/var/www/html/ReconCam"
 scp -P 57954 alert.jpg pi@192.168.1.10:$rep
 ssh pi@192.168.1.10 -p 57954 $rep/script.sh $1

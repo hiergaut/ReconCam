5ad2395a06920823a00316bc3105e56e073ee4b4
gauthier <gauthierbouyjou@aol.com>
2020-02-02 19:55:38 +0100

    update

--- main.cpp
+++ main.cpp
@@ -32,7 +32,8 @@
 #define TIMELAPSE_INTERVAL 30 // 30 sec
 #else
 // #define TIMELAPSE_INTERVAL 1800 // 30 min
-#define TIMELAPSE_INTERVAL 1200 // 20 min
+// #define TIMELAPSE_INTERVAL 1200 // 20 min
+#define TIMELAPSE_INTERVAL 600 // 10 min
 #endif
 
 #define NB_CAP_LEARNING_MODEL_FIRST 5

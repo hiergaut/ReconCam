d25898917c6d7f4157358b4fb4898be8443e69c8
gauthier <gauthierbouyjou@aol.com>
2021-12-20 22:38:51 +0100

    add mail script

---
+++ sendMailAlert.sh
@@ -0,0 +1,12 @@
+#! /bin/bash -e
+
+# echo -e 'Subject: test
+# Testing ssmtp' | sudo sendmail -v gauthierbouyjou2@gmail.com
+
+motionDir="motion/$1"
+echo "motionDir='$motionDir'"
+
+mpack -s "ReconCam alert: $1" $motionDir/trace.jpg gauthierbouyjou@aol.com
+# mpack -s "ReconCam alert: $1" $motionDir/video.webm gauthierbouyjou@aol.com
+
+# echo $1

eb2a0da2908e25c59cb433b0ebd36b49fc695e28
gauthier <gauthierbouyjou@aol.com>
2022-01-19 16:58:23 +0100

    update server

--- sendMailAlert.sh
+++ sendMailAlert.sh
@@ -1,12 +1,17 @@
 #! /bin/bash -e
 
 # echo -e 'Subject: test
-# Testing ssmtp' | sudo sendmail -v gauthierbouyjou2@gmail.com
 
 motionDir="motion/$1"
 echo "ReconCam alert: motionDir='$motionDir'"
 
-mpack -s "ReconCam alert: $1" $motionDir/trace.jpg gauthierbouyjou@aol.com
-# mpack -s "ReconCam alert: $1" $motionDir/video.webm gauthierbouyjou@aol.com
+mails=$(cat mail.txt)
+
+if [ -z "$mails" ]; then
+	echo "no mail found"
+	exit 1
+fi
+
+mpack -s "ReconCam alert: $1" $motionDir/trace.jpg $mails
 
 # echo $1

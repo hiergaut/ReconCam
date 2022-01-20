890dffd1479759a5e1e0be36894be814f4d39df6
gauthier <gauthierbouyjou@aol.com>
2022-01-20 11:12:49 +0100

    update server

--- sendMailAlert.sh
+++ sendMailAlert.sh
@@ -7,14 +7,14 @@ echo "ReconCam alert: motionDir='$motionDir'"
 
 if [ -e "sendMailEnable.txt" ]; then
 
-	mails=$(cat mails.txt)
+	mails=$(cat mails.txt | tr '\n' ' ')
 
 	if [ -z "$mails" ]; then
 		echo "no mail found"
 		exit 1
 	fi
 
-	mpack -s "ReconCam alert: $1" $motionDir/trace.jpg "$mails"
+	mpack -s "ReconCam alert: $1" $motionDir/trace.jpg $mails
 else
 	echo "sendMail is disable"
 
